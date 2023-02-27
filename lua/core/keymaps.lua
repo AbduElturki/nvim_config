d641368770d30a59c184d760fa44d3528d1eb6a0
Abdu <abdussalam@elturki.com>
2023-02-27 20:26:50 +0000

    tab spaces 4 -> 2

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
