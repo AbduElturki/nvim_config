8cd017a9ed209bfd1b1e03983a8cb7c77d29b651
Abdu Elturki <abdu.elturki@arm.com>
2024-11-19 10:35:30 +0000

    Migrate From Packer to Lazy.nvim

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -5,39 +5,26 @@ vim.g.loaded_netrwPlugin = 1
 --vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  open_on_tab = false,
-  sort_by = "case_sensitive",
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  tab = {
-    sync = {
-      open = false,
-      close = true,
-      ignore = {},
-    },
-  },
-  open_file = {
-    quit_on_open = false,
-    resize_window = true,
-    window_picker = {
-      enable = true,
-      chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-      exclude = {
-        filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-        buftype = { "nofile", "terminal", "help" },
-      },
-    },
-  },
+	open_on_tab = false,
+	sort_by = "case_sensitive",
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	tab = {
+		sync = {
+			open = false,
+			close = true,
+			ignore = {},
+		},
+	},
 })
 
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
-
+vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
